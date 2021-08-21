62d75fa0932915f7b270865342442038d4e3403d
Gia Bao <giabaosocool@gmail.com>
2021-08-22 00:16:25 +0700

    btvn buổi 4

---
+++ wi/lesson4/btvn.js
@@ -0,0 +1,28 @@
+// Câu 1. D
+// Câu 2. A
+// Câu 3. B
+// Câu 4. 
+// Câu 5. A
+// Câu 6. 
+// Câu 7. D
+// Câu 8. 
+// Câu 9
+// let m = [1,2,3,4,5,6,'hh', '9',80,100]
+// let filter = m.filter(e => typeof e === 'number')
+// let mSquared = [...filter]
+// mSquared.forEach((a,b) => {
+//     return mSquared[b] = a*a
+// })
+// console.log(filter);
+// console.log(mSquared) 
+// Câu 10
+// let strings = "High knowledge, high return"
+// let arr = strings.split(', ')
+// let ar = arr.map(e => {
+//     noWhite = e.replace(' ', '');
+//     return noWhite;
+// })
+// ar.filter((a, b) =>{
+//     ar[b] = a.toLowerCase();
+// })
+// console.log(ar);
